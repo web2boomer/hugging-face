dbc96d4190ad016c1b94cdd995b1ca593b4958fa
web2 boomer <alex@aob.io>
2024-02-21 08:33:00 -0500

    fix missing retry parameters

--- lib/hugging_face/endpoints_api.rb
+++ lib/hugging_face/endpoints_api.rb
@@ -1,6 +1,10 @@
 module HuggingFace
   class EndpointsApi < BaseApi
 
+    RETRY_WAIT = 5
+    MAX_RETRY = 60
+    
+
     def request(endpoint_url:, input:, params: nil)
       retries = 0
 
@@ -13,7 +17,7 @@ module HuggingFace
         if retries < MAX_RETRY
           logger.debug('Service unavailable, retrying...')
           retries += 1
-          sleep 5
+          sleep RETRY_WAIT
           retry
         else
           raise exception
